684196ee39fafa92bad7484d201a27b6a93d7892
ultraman231998 <anhnguyen23109898@gmail.com>
2018-11-23 19:29:47 +0700

    Initial commit

---
+++ app/src/main/java/com/example/nguyenpro231998/loginfacebook/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.nguyenpro231998.loginfacebook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.facebook.FacebookSdk;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
